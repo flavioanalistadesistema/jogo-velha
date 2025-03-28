e95219fc605a72c8f563077ba04013d1bf6a7c04
Flavio Santos <flavio.a.dos.santos@avanade.com>
2025-03-28 17:00:07 -0300

    Projeto Core - FIX

--- packages/core/test/index.test.ts
+++ packages/core/test/index.test.ts
@@ -1,4 +1,4 @@
-import { somar } from "../src";
+import { dividir, multiplicar, somar, subtrair } from "../src";
 
 test('Deve somar dois numeros', () => {
     const a = 1;
@@ -8,10 +8,34 @@ test('Deve somar dois numeros', () => {
     expect(resultado).toBe(3);
 });
 
-test('Deve somar dois numeros (2)', () => {
-    const a = 2;
+test('Deve dividir dois numeros', () => {
+    const a = 4;
     const b = 2;
-    const resultado = somar(a, b);
+    const resultado = dividir(a, b);
+    
+    expect(resultado).toBe(2);
+});
+
+test('Erro ao dividir por zero', () => {
+    const a = 4;
+    const b = 0;
+    
+    expect(() => dividir(a, b)).toThrow("Divisão por zero não é permitida.");
+})  
+
+test('Deve subtrair dois numeros', () => {
+    const a = 5;
+    const b = 2;
+    const resultado = subtrair(a, b);
     
     expect(resultado).toBe(3);
+});
+
+test('Deve multiplicar dois numeros', () => {
+    const a = 3;
+    const b = 2;
+    const resultado = multiplicar(a, b);
+    
+    expect(resultado).toBe(6);
+
 });
